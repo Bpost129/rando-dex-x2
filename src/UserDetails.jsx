13b75bfc9898d3f19fa20b89026a15c9a98132c4
Bpost129 <Bpost129@yahoo.com>
2026-01-05 14:16:36 -0500

    render user details on page, change text color

--- src/UserDetails.jsx
+++ src/UserDetails.jsx
@@ -12,8 +12,10 @@ const UserDetails = () => {
   return (
     <div className="user">
       <img src={user.personal.profilePic} alt="an AI generated image of an origami animal" />
+      <p>{user.personal.firstName} is a {user.employment.jobTitle} at {user.employment.employer}</p>
+
     </div>  
   )
 }
- 
+
 export default UserDetails
