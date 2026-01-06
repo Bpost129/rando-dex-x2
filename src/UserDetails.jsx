81a001a4daf4244c99c6dc47823237f154027939
Bpost129 <Bpost129@yahoo.com>
2026-01-05 23:11:34 -0500

    create new component files

--- src/UserDetails.jsx
+++ src/UserDetails.jsx
@@ -12,7 +12,10 @@ const UserDetails = () => {
   return (
     <div className="user">
       <img src={user.personal.profilePic} alt="an AI generated image of an origami animal" />
-      <p>{user.personal.firstName} is a {user.employment.jobTitle} at {user.employment.employer}</p>
+      <div id="user-bio">
+        <p>{user.personal.firstName} is a {user.employment.jobTitle} at {user.employment.employer}</p>
+    
+      </div>
 
     </div>  
   )
